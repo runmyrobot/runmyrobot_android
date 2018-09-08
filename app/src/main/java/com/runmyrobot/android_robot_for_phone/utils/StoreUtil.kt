cef241b1b4652b760cda2b92b0412e669952120a
Brendon Telman <b.telman01@gmail.com>
2018-09-08 04:16:10 -0500

    Setup settings backend. Add a skip-able login screen for easy robot setup

--- app/src/main/java/com/runmyrobot/android_robot_for_phone/utils/StoreUtil.kt
+++ app/src/main/java/com/runmyrobot/android_robot_for_phone/utils/StoreUtil.kt
@@ -12,6 +12,54 @@ object StoreUtil {
         return context.getSharedPreferences("robotConfig", 0)
     }
 
+    fun setRobotId(context: Context, robotId : String){
+        getSharedPrefs(context).edit().putString("robotId", robotId).apply()
+    }
+
+    fun getRobotId(context: Context) : String?{
+        return getSharedPrefs(context).getString("robotId", null)
+    }
+
+    fun setCameraId(context: Context, cameraId : String){
+        getSharedPrefs(context).edit().putString("cameraId", cameraId).apply()
+    }
+
+    fun getCameraId(context: Context) : String?{
+        return getSharedPrefs(context).getString("cameraId", null)
+    }
+
+    fun setCameraPass(context: Context, cameraId : String){
+        getSharedPrefs(context).edit().putString("cameraPass", cameraId).apply()
+    }
+
+    fun getCameraPass(context: Context) : String{
+        return getSharedPrefs(context).getString("cameraPass", "hello")
+    }
+
+    fun setMicEnabled(context: Context, micEnabled : Boolean){
+        getSharedPrefs(context).edit().putBoolean("micEnabled", micEnabled).apply()
+    }
+
+    fun getMicEnabled(context: Context) : Boolean{
+        return getSharedPrefs(context).getBoolean("micEnabled", false)
+    }
+
+    fun setTTSEnabled(context: Context, TTSEnabled : Boolean){
+        getSharedPrefs(context).edit().putBoolean("TTSEnabled", TTSEnabled).apply()
+    }
+
+    fun getTTSEnabled(context: Context) : Boolean{
+        return getSharedPrefs(context).getBoolean("TTSEnabled", false)
+    }
+
+    fun setErrorReportingEnabled(context: Context, errorReporting : Boolean){
+        getSharedPrefs(context).edit().putBoolean("errorReporting", errorReporting).apply()
+    }
+
+    fun getErrorReportingEnabled(context: Context) : Boolean{
+        return getSharedPrefs(context).getBoolean("errorReporting", false)
+    }
+
     fun SetBluetoothDevice(context: Context, name : String, address : String) {
         getSharedPrefs(context).edit().putString("BTName", name).apply()
         getSharedPrefs(context).edit().putString("BTAddress", address).apply()
