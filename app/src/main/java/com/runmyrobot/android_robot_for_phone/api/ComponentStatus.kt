bb78ea2624bb2c70c341b10bbf3b05d0de14ed36
Brendon Telman <b.telman01@gmail.com>
2018-09-17 23:44:34 -0500

    Add in a component status enum that the indicators will use

---
+++ app/src/main/java/com/runmyrobot/android_robot_for_phone/api/ComponentStatus.kt
@@ -0,0 +1,13 @@
+package com.runmyrobot.android_robot_for_phone.api
+
+/**
+ * Created by Brendon on 9/17/2018.
+ */
+enum class ComponentStatus {
+    DISABLED,
+    IDLE,
+    CONNECTING,
+    STABLE,
+    INTERMITTENT,
+    ERROR
+}
